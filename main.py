bc9b3d1aeb9d6f3ba6575604c2286545a9b52119
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-06 12:43:18 +0300

    2.2.6

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-class Car:
-    def __init__(self):
-        self.__model = ''
-        
+class WindowDlg:
+    def __init__(self, title, width, height):
+        self.__title = title
+        self.__width = width
+        self.__height = height
+
+    def show(self):
+        print(f'{self.__title}: {self.width}, {self.height}')
+
     @property
-    def model(self):
-        return self.__model
-    
-    @model.setter
-    def model(self, value):
-        if isinstance(value, str) and 2 <= len(value) <= 100:
-            self.__model = value
+    def width(self):
+        return self.__width
 
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) and 0 <= value <= 1000:
+            self.__width = value
+            self.show()
 
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) and 0 <= value <= 10000:
+            self.__height = value
+            self.show()
