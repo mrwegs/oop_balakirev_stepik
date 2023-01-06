73fc313efacb2926c5a18fc99451fdd9c12bc434
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-06 12:29:18 +0300

    2.2.5

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-import random as r
-from string import ascii_lowercase, ascii_uppercase, digits
-import re
+class Car:
+    def __init__(self):
+        self.__model = ''
+        
+    @property
+    def model(self):
+        return self.__model
+    
+    @model.setter
+    def model(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 100:
+            self.__model = value
 
 
-class EmailValidator:
-    ALLOW_SYMBOLS = ascii_uppercase + ascii_lowercase + digits + '.' + '_'
-
-    __instance = None
-
-    def __new__(cls, *args, **kwargs):
-        return cls.__instance
-
-    @classmethod
-    def get_random_email(cls):
-        n = r.randint(1, 20)
-        name = r.choices(cls.ALLOW_SYMBOLS, k=n)
-        return ''.join(name) + '@gmail.com'
-
-    @classmethod
-    def check_email(cls, email):
-        if not cls.__is_email_str(email):
-            return False
-        pattern = r'((\w)(\.?)){1,100}@(\w+\.[a-z]+){1,50}'
-        match = re.fullmatch(pattern, email)
-        return bool(match)
-
-    @staticmethod
-    def __is_email_str(email):
-        return isinstance(email, str)
