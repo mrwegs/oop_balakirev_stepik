7422f46e5292314038893229610f3fac31b0a273
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-07 15:21:28 +0300

    2.2.7

--- main.py
+++ main.py
@@ -1,28 +1,65 @@
-class WindowDlg:
-    def __init__(self, title, width, height):
-        self.__title = title
-        self.__width = width
-        self.__height = height
-
-    def show(self):
-        print(f'{self.__title}: {self.width}, {self.height}')
+class StackObj:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
 
     @property
-    def width(self):
-        return self.__width
+    def data(self):
+        return self.__data
 
-    @width.setter
-    def width(self, value):
-        if isinstance(value, int) and 0 <= value <= 1000:
-            self.__width = value
-            self.show()
+    @data.setter
+    def data(self, value):
+        self.__data = value
 
     @property
-    def height(self):
-        return self.__height
+    def next(self):
+        return self.__next
 
-    @height.setter
-    def height(self, value):
-        if isinstance(value, int) and 0 <= value <= 10000:
-            self.__height = value
-            self.show()
+    @next.setter
+    def next(self, value):
+        if isinstance(value, StackObj) or value is None:
+            self.__next = value
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, obj: StackObj):
+        if self.top:
+            check_obj = self.top
+            while check_obj.next is not None:
+                check_obj = check_obj.next
+            check_obj.next = obj
+        else:
+            self.top = obj
+
+    def pop(self):
+        if self.top and self.top.next:
+            first = self.top
+            second = first.next
+            while second and second.next is not None:
+                first = second
+                second = first.next
+            pop_item = second
+            first.next = None
+            return pop_item
+        pop_item = self.top
+        self.top = None
+        return pop_item
+
+    def get_data(self):
+        res = list()
+        check_obj = self.top
+        while check_obj is not None:
+            res.append(check_obj.data)
+            check_obj = check_obj.next
+
+        return res
+
+
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.pop()
+assert s.get_data() == [], f"метод get_data вернул неверные данные: {s.get_data()}"
