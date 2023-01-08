61acbb7c9f17a0d04861dbdb12d80bb95b6cd823
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-08 14:08:20 +0300

    2.3.7

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-class PhoneNumber:
-    def __init__(self, number: int, fio: str):
-        self.number = number if self.check_phone(number) else 00000000000
-        self.fio = fio
-
+class FloatValue:
     @staticmethod
-    def check_phone(phone: int):
-        return len(str(phone)) == 11 and isinstance(phone, int)
+    def validate_float(value):
+        if not isinstance(value, float):
+            raise TypeError("Присваивать можно только вещественный тип данных.")
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        self.validate_float(value)
+        setattr(instance, self.name, value)
 
 
-class PhoneBook:
-    def __init__(self):
-        self.phone_list = list()
+class Cell:
+    value = FloatValue()
 
-    def add_phone(self, phone: PhoneNumber):
-        self.phone_list.append(phone)
+    def __init__(self, value):
+        self.value = value
 
-    def remove_phone(self, indx):
-        self.phone_list.pop(indx)
 
-    def get_phone_list(self):
-        return self.phone_list
+class TableSheet:
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        self.cells = [[Cell(0.0)] * m] * n
 
+
+table = TableSheet(5, 3)
+table.cells = [[Cell(float(i * table.m + j)) for j in range(1, table.m + 1)] for i in range(table.n)]
