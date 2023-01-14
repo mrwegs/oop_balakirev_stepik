355d764239edab899933e9fe73d2f4855b7aafa6
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-14 16:07:51 +0300

    2.3.10

--- main.py
+++ main.py
@@ -1,50 +1,32 @@
-class StringValue:
-    def __init__(self, min_length = 2, max_length = 50):
-        self.min = min_length
-        self.max = max_length
-
-    def __set_name__(self, owner, name):
-        self.name = '_' + name
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.name)
-
-    def __set__(self, instance, value):
-        if isinstance(value, str) and len(value) in range(self.min, self.max + 1):
-            setattr(instance, self.name, value)
-
-
-class PriceValue:
-    def __init__(self, max_value = 10000):
-        self.max = max_value
-
-    def __set_name__(self, owner, name):
-        self.name = '_' + name
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.name)
-
-    def __set__(self, instance, value):
-        if isinstance(value, (int, float)) and value in range(0, self.max + 1):
-            setattr(instance, self.name, value)
-
-
-class SuperShop:
-    def __init__(self, name):
+class Thing:
+    def __init__(self, name, weight):
         self.name = name
-        self.goods = []
-
-    def add_product(self, product):
-        self.goods.append(product)
-
-    def remove_product(self, product):
-        self.goods.remove(product)
+        self.weight = weight
 
 
-class Product:
-    name = StringValue()
-    price = PriceValue()
+class Bag:
+    def __init__(self, max_weight):
+        self.max_weight = max_weight
+        self.__things = []
 
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
+    def get_total_weight(self):
+        if self.things:
+            return sum([thing.weight for thing in self.__things])
+        return 0
+
+    @property
+    def things(self):
+        return self.__things
+
+    @things.setter
+    def things(self, value):
+        self.__things = value
+
+    def add_thing(self, thing: Thing):
+        if self.get_total_weight() + thing.weight <= self.max_weight:
+            self.things.append(thing)
+
+    def remove_thing(self, indx):
+        if self.things:
+            self.things.pop(indx)
+
