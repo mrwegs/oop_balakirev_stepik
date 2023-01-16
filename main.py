b6ce4db76a80bb18cf4e73844ce722eb8e7589db
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-16 22:02:18 +0300

    3.1.9

--- main.py
+++ main.py
@@ -1,51 +1,52 @@
-class AppVK:
-    __instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            return super().__new__(cls)
-        return cls.__instance
-
-    def __init__(self, name='ВКонтакте'):
-        self.name = name
+from typing import Union
 
 
-class AppYouTube:
-    __instance = None
+class Circle:
+    __x: Union[int, float]
+    __y: Union[int, float]
+    __radius: Union[int, float]
 
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            return super().__new__(cls)
-        return cls.__instance
+    def __init__(self, x, y, radius):
+        self.__x = x
+        self.__y = y
+        self.__radius = radius
 
-    def __init__(self, max_size, name='YouTube'):
-        self.name = name
-        self.max_size = max_size
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        self.__radius = value
+
+    def __setattr__(self, key, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError('Неверный тип присваиваемых данных.')
+        elif key == 'radius' and value < 0:
+            return
+        super().__setattr__(key, value)
+
+    def __getattr__(self, item):
+        return False
 
 
-class AppPhone:
-    __instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            return super().__new__(cls)
-        return cls.__instance
-
-    def __init__(self, phone_list, name='Phone'):
-        self.name = name
-        self.phone_list = phone_list
-
-
-class SmartPhone:
-    model: str
-    apps: list
-
-    def __init__(self, model):
-        self.model = model
-
-    def add_app(self, app):
-        self.apps.append(app)
-
-    def remove_app(self, app):
-        self.apps.remove(app)
-
+circle = Circle(10.5, 7, 22)
+circle.radius = -10 # прежнее значение не должно меняться, т.к. отрицательный радиус недопустим
+x, y = circle.x, circle.y
+res = circle.name # False, т.к. атрибут name не существует
