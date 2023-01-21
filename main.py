7cb800055e71950672041286edf4f1df8326129e
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-21 23:01:50 +0300

    3.2.8

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-class RenderList:
-    type_list: str
+class HandlerGET:
+    def __init__(self, func):
+        self.func = func
 
-    def __init__(self, type_list):
-        self.type_list = type_list
-
-    def __setattr__(self, key, value):
-        if key == 'type_list' and value not in ('ul', 'ol'):
-            self.__dict__[key] = 'ul'
-        else:
-            super().__setattr__(key, value)
-
-    def __call__(self, *args, **kwargs):
-        string_list = args[0]
-        string = '</li>\n<li>'.join(string_list)
-        return f'<{self.type_list}>\n<li>{string}</li>\n</{self.type_list}>'
-
+    def __call__(self, request, *args, **kwargs):
+        method = request.get('method', None)
+        return f'GET: {self.func(request)}' if method in ('GET', None) else None
 
