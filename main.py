5851942048af7afef4efeda3393dd343aaade4c0
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-21 13:54:55 +0300

    3.2.3

--- main.py
+++ main.py
@@ -1,57 +1,26 @@
-import time
+from random import randint # функция для генерации целых случайных значений в диапазоне [a; b]
 
 
-class WaterFilter:
-    date: float
+class RandomPassword:
+    psw_chars: str
+    min_length: int
+    max_length: int
 
-    def __init__(self, date):
-        self.date = date
+    def __init__(self, psw_chars, min_length, max_length):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
 
-    def __setattr__(self, key, value):
-        if key == 'date' and key in self.__dict__.keys():
-            return
-        super().__setattr__(key, value)
-
-
-class Mechanical(WaterFilter):
-    pass
-
-
-class Aragon(WaterFilter):
-    pass
+    def __call__(self, *args, **kwargs):
+        psw_length = randint(self.min_length, self.max_length)
+        password = ''.join([self.psw_chars[randint(0, len(self.psw_chars)) - 1] for _ in range(psw_length)])
+        return password
 
 
-class Calcium(WaterFilter):
-    pass
+rnd = RandomPassword('qwertyuiopasdfghjklzxcvbnm0123456789', 5, 20)
+
+lst_pass = [rnd() for _ in range(3)]
 
 
-class GeyserClassic:
-    MAX_DATE_FILTER: int = 100
 
-    slot_1: Mechanical
-    slot_2: Aragon
-    slot_3: Calcium
 
-    all_filters: dict
-
-    def __init__(self):
-        self.all_filters = {'1': None, '2': None, '3': None}
-
-    def __setattr__(self, key, value):
-        super().__setattr__(key, value)
-
-    def add_filter(self, slot_num, filter):
-        if self.all_filters[str(slot_num)] is None and isinstance(filter, self.__annotations__.get(f'slot_{slot_num}')):
-            self.all_filters[str(slot_num)] = filter
-
-    def remove_filter(self, slot_num):
-        self.all_filters[str(slot_num)] = None
-
-    def get_filters(self):
-        return self.all_filters.values()
-
-    def water_on(self):
-        for water_filter in self.all_filters.values():
-            if not water_filter or time.time() - water_filter.date > self.MAX_DATE_FILTER:
-                return False
-        return True
