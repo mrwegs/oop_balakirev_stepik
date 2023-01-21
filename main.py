8aebcf580d60b9eddd4621b08bba62f4b0c75b88
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-21 16:15:07 +0300

    3.2.7

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-class DigitRetrieve:
+class RenderList:
+    type_list: str
+
+    def __init__(self, type_list):
+        self.type_list = type_list
+
+    def __setattr__(self, key, value):
+        if key == 'type_list' and value not in ('ul', 'ol'):
+            self.__dict__[key] = 'ul'
+        else:
+            super().__setattr__(key, value)
+
     def __call__(self, *args, **kwargs):
-        number = args[0]
-        try:
-            return int(number)
-        except ValueError:
-            return None
+        string_list = args[0]
+        string = '</li>\n<li>'.join(string_list)
+        return f'<{self.type_list}>\n<li>{string}</li>\n</{self.type_list}>'
+
 
