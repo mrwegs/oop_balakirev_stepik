443751db04ef4348d2e84eee7ad3ed27ca1796c2
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-22 22:16:13 +0300

    3.2.9

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-class HandlerGET:
-    def __init__(self, func):
-        self.func = func
+class Handler:
+    def __init__(self, methods):
+        self.methods = methods
 
-    def __call__(self, request, *args, **kwargs):
-        method = request.get('method', None)
-        return f'GET: {self.func(request)}' if method in ('GET', None) else None
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            method = args[0].get('method', None) or 'GET'
+            if method in self.methods:
+                return self.__getattribute__(f'{method.lower()}')(func, args[0])
+            return None
 
+        return wrapper
+
+    @staticmethod
+    def get(func, request, *args, **kwargs):
+        return f'GET: {func(request)}'
+
+    @staticmethod
+    def post(func, request, *args, **kwargs):
+        return f'POST: {func(request)}'
