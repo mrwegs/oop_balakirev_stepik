4545a7a7bb8dd0d46783fad32b0ad2c91453a458
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-01-29 15:16:32 +0300

    3.4.5

--- main.py
+++ main.py
@@ -1,104 +1,29 @@
-from typing import Union, Any
+from collections import Counter
 
 
-class ObjList:
-    __data: str
-    __prev: Any
-    __next: Any
+class NewList:
+    def __init__(self, lst: list = []):
+        self.lst = lst
 
-    def __init__(self, data):
-        self.__data = data
-        self.__prev = None
-        self.__next = None
+    def __sub__(self, other):
+        if not isinstance(other, (list, NewList)):
+            raise TypeError('Некорректный тип данных')
+        other = other.lst if isinstance(other, self.__class__) else other
+        other_types = [(n, type(n)) for n in other]
+        result_list = list()
+        for el in map(lambda x: (x, type(x)), self.lst):
+            if el in other_types:
+                other_types.remove(el)
+            else:
+                result_list.append(el[0])
 
-    @property
-    def data(self):
-        return self.__data
+        return self.__class__(result_list)
 
-    @data.setter
-    def data(self, value):
-        self.__data = value
+    def __rsub__(self, other):
+        if not isinstance(other, (list, NewList)):
+            raise TypeError('Некорректный тип данных')
+        other = other if isinstance(other, self.__class__) else self.__class__(other)
+        return other - self
 
-    @property
-    def prev(self):
-        return self.__prev
-
-    @prev.setter
-    def prev(self, value):
-        self.__prev = value
-
-    @property
-    def next(self):
-        return self.__next
-
-    @next.setter
-    def next(self, value):
-        self.__next = value
-
-
-class LinkedList:
-    __head: Union[ObjList, None]
-    __tail: Union[ObjList, None]
-    __objects: list
-
-    def __init__(self):
-        self.__head = None
-        self.__tail = None
-        self.__objects = list()
-        
-    @property
-    def head(self):
-        return self.__head
-    
-    @head.setter
-    def head(self, value):
-        self.__head = value
-        
-    @property
-    def tail(self):
-        return self.__tail
-    
-    @tail.setter
-    def tail(self, value):
-        self.__tail = value
-
-    @property
-    def objects(self):
-        return self.__objects
-
-    @objects.setter
-    def objects(self, value):
-        self.__objects = value
-
-    def add_obj(self, obj: ObjList):
-        self.objects.append(obj)
-
-        if self.head is None:
-            self.head = self.tail = obj
-        else:
-            self.tail.next = obj
-            obj.prev = self.tail
-            self.tail = obj
-
-    def remove_obj(self, indx):
-        obj: ObjList = self.objects[indx]
-        if len(self.objects) == 1:
-            self.head = self.tail = None
-        elif obj is self.tail:
-            self.tail = self.tail.prev
-            self.tail.next = None
-        elif obj is self.head:
-            self.head = obj.next
-            self.head.prev = None
-        else:
-            obj.prev.next = obj.next
-            obj.next.prev = obj.prev
-
-        self.objects.pop(indx)
-
-    def __len__(self):
-        return len(self.objects)
-
-    def __call__(self, *args, **kwargs):
-        return self.objects[args[0]].data
-
+    def get_list(self):
+        return self.lst
