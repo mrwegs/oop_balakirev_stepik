23f58be2e117839128b26c74561b7f0e79813073
Евгений Стороженко <eugene@MacBook-Air-Evgenij.local>
2023-02-05 17:50:43 +0300

    3.7.5

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-class Rect:
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+import sys
 
-    def __hash__(self):
-        return hash((self.width, self.height))
+
+class Player:
+    name: str
+    old: int
+    score: int
+
+    def __init__(self, name, old, score):
+        self.name = name
+        self.old = old
+        self.score = score
+
+    def __bool__(self):
+        return self.score > 0
+
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+
+players = [Player(*map(lambda x: int(x) if x.isdigit() else x, string.split('; '))) for string in lst_in]
+
+players_filtered = list(filter(bool, players))
